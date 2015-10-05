ab2a22173d2e366c54ebcce1d19ec372cbf48583
Murali Veeraiyan <murali.veeraiyan@gmail.com>
2015-10-05 21:40:36 +0530

    Add admin namespace with application controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ unless User.exists?(email: "viewer@ticketee.com")
 	User.create!(email: "viewer@ticketee.com", password: "password")
 end
 
-%w[Sublime Text 3, Internet Explorer].each do |name|
+["Sublime Text 3", "Internet Explorer"].each do |name|
 	unless Project.exists?(name: name)
 		Project.create!(name: name, description: "A sample project about #{name}")
 	end
